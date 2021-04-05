ace86627f82299a3784ac1b60771849f714f132d
ByMartrixx <bymartrixx@gmail.com>
2021-04-05 18:48:26 -0400

    Fix

--- src/main/java/me/bymartrixx/booklib/BookLib.java
+++ src/main/java/me/bymartrixx/booklib/BookLib.java
@@ -1,4 +1,4 @@
-package me.bymartrixx.templatemodpkg;
+package me.bymartrixx.booklib;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
@@ -6,10 +6,10 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class TemplateModClass implements ModInitializer {
-	public static final String MOD_ID = "template_mod";
-	public static final String MOD_NAME = "Template_Mod_Name";
-	private static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).isPresent() ? FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().toString() : "template-mod-ver";
+public class BookLib implements ModInitializer {
+	public static final String MOD_ID = "booklib";
+	public static final String MOD_NAME = "Book Lib";
+	private static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).isPresent() ? FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().toString() : "1.0.0";
 
 	public static final Logger LOGGER = LogManager.getLogger();
 
